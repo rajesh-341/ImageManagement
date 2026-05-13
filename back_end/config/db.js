0fc31c19fee89a1489576aca0fdc82501fbe5ad1
Rajesh Balaji <rajeshbalaaji@gmail.com>
2026-05-13 22:14:29 +0530

    backendIssueFix2

--- back_end/config/db.js
+++ back_end/config/db.js
@@ -6,6 +6,8 @@ const pool = new Pool({
   database: process.env.DB_NAME || 'image_metadata',
   password: process.env.DB_PASSWORD || '',
   port: parseInt(process.env.DB_PORT || '5432'),
+  ssl: { rejectUnauthorized: false },
+  family: 4,
 });
 
 module.exports = pool;
